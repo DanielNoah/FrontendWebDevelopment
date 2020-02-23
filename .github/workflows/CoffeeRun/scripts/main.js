0548733270836c591ec450b81a3f0fa5425e75c3
DanielNoah <gonoah8@gmail.com>
2020-02-23 14:07:16 +0900

    Add files via upload

--- .github/workflows/CoffeeRun/scripts/main.js
+++ .github/workflows/CoffeeRun/scripts/main.js
@@ -1,8 +1,19 @@
 (function(window) {
     'user strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
     var App = window.App;
     var Cafe = App.Cafe;
     var DataStore = App.DataStore;
+    var FormHandler = App.FormHandler;
+    var CheckList = App.CheckList;
     var myCafe = new Cafe('ncc-1801', new DataStore());
-    window.myCafe = myCafe;    
+    window.myCafe = myCafe;   
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(function (data) {
+                                    myCafe.createOrder.call(myCafe, data);
+                                    checkList.addRow.call(checkList, data);
+                                });
 })(window);
