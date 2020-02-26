dc75f8baeb7827b36c25d819510cf7ae5a40acbc
DanielNoah <gonoah8@gmail.com>
2020-02-26 16:03:32 +0900

    Add files via upload

--- CoffeeRun/scripts/FormHandler.js
+++ CoffeeRun/scripts/FormHandler.js
@@ -15,6 +15,7 @@
     }
 
     FormHandler.prototype.addSubmitHandler = function (fn) {
+
       console.log('Setting submit handler for form');
       this.$formElement.on('submit', function (event) {
         event.preventDefault();
@@ -25,9 +26,24 @@
         console.log(item.name + ' is ' + item.value);
       });
       console.log(data);
+      
       fn(data);
       this.reset();
       this.elements[0].focus();
+    });
+  };
+
+  FormHandler.prototype.addInputHandler = function (fn) {
+    console.log('Setting input handler for form');
+    this.$formElement.on('input', '[name="emailAddress"]', function (event) {
+      var emailAddress = event.target.value;
+      var message = '';
+      if (fn(emailAddress)) {
+        event.target.setCustomValidity('');
+      } else {
+        message = emailAddress + ' is not an authorized email address!'
+        event.target.setCustomValidity(message);   
+      }
     });
   };
 
